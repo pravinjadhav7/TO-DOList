ad176eebfe642598d3a4009f6946bb41eb1947eb
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-08-11 13:39:22 +0530

    updating css and html @angular2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,10 @@ import {TodoDataService} from './todo-data.service';
   styleUrls: ['./app.component.css'],
   providers: [TodoDataService]
 })
-export class AppComponent {
- 
-  newTodo: Todo = new Todo();
 
+export class AppComponent {
+  
+  newTodo: Todo = new Todo();
   constructor(private todoDataService: TodoDataService) {
   }
 
@@ -20,11 +20,6 @@ export class AppComponent {
     this.todoDataService.addTodo(this.newTodo);
     this.newTodo = new Todo();
   }
-
-  // toggleTodoComplete(todo) {
-  //   this.todoDataService.toggleTodoComplete(todo);
-  // }
-
   removeTodo(todo) {
     this.todoDataService.deleteTodoById(todo.id);
   }
